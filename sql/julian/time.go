a168857f5d735679f7d16b94a45f5fdc38a3314c
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-22 20:38:45 +0000

    update consts

--- sql/julian/time.go
+++ sql/julian/time.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	secondsInADay      = 86400
+	DaySeconds         = 86400
 	UnixEpochJulianDay = 2440587.5
 )
 
@@ -13,12 +13,12 @@ type Time float64
 
 // AsTime converts a Julian day into a time.Time.
 func (t Time) AsTime() time.Time {
-	return time.Unix(int64((t-UnixEpochJulianDay)*secondsInADay), 0).UTC()
+	return time.Unix(int64((t-UnixEpochJulianDay)*DaySeconds), 0).UTC()
 }
 
 // FromTime converts a time.Time into a Julian day.
 func FromTime(t time.Time) Time {
-	return Time(float64(t.UTC().Unix())/secondsInADay + UnixEpochJulianDay)
+	return Time(float64(t.UTC().Unix())/DaySeconds + UnixEpochJulianDay)
 }
 
 func Now() Time {
