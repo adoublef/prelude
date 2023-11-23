65798024dbcc89632c816b7f11de82cdd3cc4688
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-23 13:18:18 +0000

    tx testing

--- sql/db_test.go
+++ sql/db_test.go
@@ -39,4 +39,31 @@ func TestSQL(t *testing.T) {
 		is.NoErr(err)        // query
 		is.Equal(bad, false) // does not exists
 	}))
+
+	t.Run("tx.query", st.RoundTrip(fsys, func(t *testing.T, db *sql.DB) {
+		var (
+			is  = is.New(t)
+			ctx = context.Background()
+			tx, err = db.Begin()
+		)
+		is.NoErr(err) // tx
+		defer tx.Rollback()
+
+		err = tx.QueryRow(ctx, "SELECT * FROM t WHERE t.a = 1").Err()
+		is.NoErr(err) // exists
+
+		var n int
+		err = tx.QueryRow(ctx, "SELECT * FROM t WHERE t.a = 2").Scan(&n)
+		is.Equal(err, sql.ErrNoRows) // not exist
+
+		var ok bool
+		err = tx.QueryRow(ctx, "SELECT EXISTS (SELECT * FROM t WHERE t.a = 1)").Scan(&ok)
+		is.NoErr(err)      // query
+		is.Equal(ok, true) // exists
+
+		var bad bool
+		err = tx.QueryRow(ctx, fmt.Sprintf("SELECT EXISTS (%s)", "SELECT * FROM t WHERE t.a = 2")).Scan(&bad)
+		is.NoErr(err)        // query
+		is.Equal(bad, false) // does not exists
+	}))
 }
